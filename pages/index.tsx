b6e7d1a587466150f6e956b25974380e0714b06f
JulioCesarASilva <juliodragon1998@gmail.com>
2023-01-08 01:03:23 -0300

    feat: add components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
 import Head from 'next/head'
+import Header from '../public/components/Header'
+import Particles from '../public/components/Particles'
+import TextAnimation from '../public/components/TextAnimation'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
   return (
     <>
       <Head>
         <title>Julio Cesar</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={styles.main}>
-        Ola Mundo!
+      <Header />
+      <main>
+        <section className={styles.particles}>
+          <Particles />
+          <div className={styles.info}>
+            <h1>Julio Cesar</h1>
+            <p>
+              I'm
+              <TextAnimation />
+            </p>
+
+          </div>
+        </section>
+        <section>
+          OLA Mundo
+        </section>
       </main>
     </>
   )
