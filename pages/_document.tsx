bd984dd3ccbf51329a00818088fa62ce953e0f77
JulioCesarASilva <juliodragon1998@gmail.com>
2023-02-01 00:25:32 -0300

    feat: use random in index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useState, useMemo } from "react"
 
 export default function Document() {
-  const [color, setColor] = useState<number | null>(null)
-
-    useMemo(() => {
-        setColor(Math.floor(Math.random() * 3))
-    }, [])
-
   return (
     <Html lang="en">
       <Head />
-      <body data-color={`COLOR-${color?.toString()}`}>
+      <body>
         <Main />
         <NextScript />
       </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
-import Head from 'next/head'
-import Header from '../Components/Header'
-import Particles from '../Components/Particles'
-import TextAnimation from '../Components/TextAnimation'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Header from "../Components/Header";
+import Particles from "../Components/Particles";
+import TextAnimation from "../Components/TextAnimation";
+import styles from "../styles/Home.module.css";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (document) {
+      document.body.setAttribute(
+        "data-color",
+        `COLOR-${Math.floor(Math.random() * 3)}`
+      );
+    }
+  }, []);
   return (
     <>
       <Head>
@@ -13,21 +23,21 @@ export default function Home() {
       </Head>
       <Header />
       <main>
-        <section id='inicio' className={styles.particles}>
+        <section id="inicio" className={styles.particles}>
           <Particles />
           <div className={styles.info}>
             <h1>Julio Cesar</h1>
+
             <p>
               I&apos;m
               <TextAnimation />
             </p>
-
           </div>
         </section>
-        <section id="sobre" style={{height: "100vh", paddingTop: "70px"}}>
+        <section id="sobre" style={{ height: "100vh", paddingTop: "70px" }}>
           OLA Mundo
         </section>
       </main>
     </>
-  )
+  );
 }
