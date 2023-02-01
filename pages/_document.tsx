bc16c080db150dc74177fe1c3ef83d86e0c0ffc5
JulioCesarASilva <juliodragon1998@gmail.com>
2023-01-31 21:59:45 -0300

    feat: add useMemo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import {useMemo, useState} from "react"
 
 export default function Document() {
-
+  const [color, setColor] = useState<number>()
+  useMemo(()=> {
+    setColor(Math.floor(Math.random() * 3))
+  }, [])
 
   return (
     <Html lang="en">
       <Head />
-      <body data-color={`COLOR-${Math.floor(Math.random() * 3)}`}>
+      <body data-color={`COLOR-${color}`}>
         <Main />
         <NextScript />
       </body>
