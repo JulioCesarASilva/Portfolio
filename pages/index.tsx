22dfaa2479d88ce6c10efe4c628b782bce727e9f
JulioCesarASilva <juliodragon1998@gmail.com>
2023-02-01 21:55:39 -0300

    feat: add sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,8 +34,33 @@ export default function Home() {
             </p>
           </div>
         </section>
-        <section id="sobre" style={{ height: "100vh", paddingTop: "70px" }}>
-          OLA Mundo
+        <section id="sobre" className={`${styles.section}`}>
+          <h2>Sobre Mim</h2>
+          <div>
+            Sou um desenvolvedor web altamente capacitado e apaixonado por criar soluções digitais inovadoras. Tenho ampla experiência em desenvolvimento de aplicativos e sites, incluindo a implementação de recursos avançados como animações, integrações com APIs e otimização para motores de busca.
+            <br/>
+            Durante minha carreira, desenvolvi habilidades em diversas tecnologias de desenvolvimento web, incluindo HTML, CSS, JavaScript e frameworks como React e Angular. Além disso, trabalhei em projetos de equipe, o que me permitiu desenvolver minhas habilidades de comunicação e colaboração.
+            <br/>
+            Acredito que minha experiência e habilidades fazem de mim um candidato forte para o mercado de desenvolvimento web. Estou ansioso para continuar aplicando meus conhecimentos em novos projetos e desafios, e sempre buscando oportunidades de aprendizado para me tornar um desenvolvedor ainda mais eficaz.
+            <br/>
+            Minha meta é criar soluções digitais que possam ajudar a melhorar a vida das pessoas e tornar o mundo um lugar melhor. Estou sempre buscando novos desafios e oportunidades para crescer profissionalmente e alcançar meus objetivos.
+          </div>
+        </section>
+        <section id="tecnologia" className={`${styles.section}`}>
+          <h2>Tecnologias</h2>
+          <div>Bla...  Bla... Bla...</div>
+        </section>
+        <section id="tragetoria" className={`${styles.section}`}>
+          <h2>Tragetoria</h2>
+          <div>Bla...  Bla... Bla...</div>
+        </section>
+        <section id="projetos" className={`${styles.section}`}>
+          <h2>Projetos</h2>
+          <div>Bla...  Bla... Bla...</div>
+        </section>
+        <section id="contato" className={`${styles.section}`}>
+          <h2>Contato</h2>
+          <div>Bla...  Bla... Bla...</div>
         </section>
       </main>
     </>
